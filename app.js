216a9436065f637882b76cd1f7cc9f5e82b05cab
Mahesh <thapa.mahesh08101998@gmail.com>
2025-12-11 18:57:53 +0530

    add console.log

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => {
+  console.log("GET /")
   res.send('Hello World!')
 })
 
 app.get("/login",(req,res)=>{
+  console.log("GET /login")
   res.send("<h1>Please login at chai aur code.</h1>")
 })
 
 app.get("/youtube",(req,res)=>{
+    console.log("GET /youtube")
     res.send("<h2>Chai Aur Code</h2>")
-})
-app.listen(port, () => {
+  })
+  app.listen(port, () => {
+  console.log("GET /listen")
   console.log(`Example app listening on port ${port}`)
 })
